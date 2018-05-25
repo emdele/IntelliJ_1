96e13d71ba96bab5282eb09f3284e31e60ca2352
Ayo <31580516+emdele@users.noreply.github.com>
2018-05-25 03:04:04 +0100

    Testing

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
 package com.ea;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ayodeji\\Desktop\\chromedriver.exe\\");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.florida4less.co.uk/");
+        driver.findElement(By.xpath("//*[@id=\"layout\"]/div[1]/header/div[4]/nav/ul/li[1]/a"));
+        driver.findElement(By.xpath("//*[@id=\"layout\"]/div[1]/header/div[4]/nav/ul/li[2]/a"));
+        driver.close();
 
-        System.out.println("hello world");
-        System.out.println("hello there!Lorem Ipsum is simply dummy text of the" +
-                " printing and typesetting industry. Lorem Ipsum ");
+
+
     }
 
 
