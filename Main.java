e76ed767df5b8e45334d21cf1a868c5ade7090f0
Ayo <31580516+emdele@users.noreply.github.com>
2018-05-25 02:40:04 +0100

    IntelliJ

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.ea;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello world");
+        System.out.println("hello there!Lorem Ipsum is simply dummy text of the" +
+                " printing and typesetting industry. Lorem Ipsum ");
+    }
+
+
+}
